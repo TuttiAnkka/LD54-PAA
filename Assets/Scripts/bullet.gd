ea1c69e7d48e8a603aa3b44b89f7df775a5514e8
TuttiAnkka <47348410+TuttiAnkka@users.noreply.github.com>
2023-09-30 17:27:39 +0300

    bullet destroy timer

--- Assets/Scripts/bullet.gd
+++ Assets/Scripts/bullet.gd
@@ -7,9 +7,12 @@ var manager = null
 
 func _enter_tree():
 	manager = get_node("/root/Main/GameManager")
+	await get_tree().create_timer(10).timeout # Destroy timer.
+	queue_free()
 
 func _physics_process(delta):
 	position += direction * speed * delta
+	
 
 func _on_body_entered(body):
 	if body.is_in_group("Enemy") && !damage_player:
